73d2f954fe09dc243dd274218518c77bea756602
MrSriJay <jayanga.sl@gmail.com>
2025-12-07 01:55:12 +0800

    config changes added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
@@ -7,22 +8,31 @@ import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST ,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER ,
-      password: process.env.DB_PASS ,
-      database: process.env.DB_NAME ,
-
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
-
-      ssl: {
-        rejectUnauthorized: false,
-      },
+    // Load .env.local explicitly
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true, 
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<number>('DB_PORT')),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        ssl: { 
+          rejectUnauthorized: false,
+        }
+      }),
+    }),
+    
 
     OrderModule,
     ProductModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Order Processing API';  // You can move the logic here if needed
   }
 }
